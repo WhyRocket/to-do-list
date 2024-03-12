5fc76bd77929524fe304481f3a7b90ad4d6784a0
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-03-12 23:28:28 +0500

    Создал решение

---
+++ src/To-do-list/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace To_do_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
