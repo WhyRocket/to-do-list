505c4a5586f6acd3f6382b50f532190b604d3dad
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-03-15 19:52:58 +0500

    Внес правки

---
+++ src/ToDoList/ToDoList.Entity/Enums/CardStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoList.Entity.Enums;
+
+// Перечисление возможных статусов задач.
+public enum CardStatus
+{
+    // Выполнено.
+    Done = 1,
+
+    // Не выполнено.
+    NotDone = 2,
+
+    // Отложено.
+    Postoned = 3,
+
+    // Начато.
+    Started = 4
+}
