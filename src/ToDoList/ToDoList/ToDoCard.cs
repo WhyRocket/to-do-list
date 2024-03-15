47b4ed1d2ff0c849cbd90b08f5106e44fc21fb52
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-03-15 16:07:54 +0500

    Создал класс, описывающий карточку дела

---
+++ src/ToDoList/ToDoList/ToDoCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList;
+
+internal class ToDoCard
+{
+    // Этот класс описывает карточку задачи в "Списке дел".
+
+    public int TaskNumber { get; set; }
+    public string TaskName { get; set; }
+    public string TaskInfo { get; set; }
+    public DateTime? TaskDate { get; set; }
+    public Status TaskStatus { get; set; }
+
+
+    // Перечисление возможных статусов задач.
+    public enum Status
+    {
+        Done,       // Выполнено.
+        NotDone,    // Не выполнено.
+        Postoned,   // Отложено
+        Started     // Начато.
+    }
+}
